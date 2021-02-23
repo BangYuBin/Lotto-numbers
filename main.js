174c87b43f62852cf35f1e006be6ce1a86bf18a9
Tori <ttang1135@naver.com>
2021-02-23 14:02:07 +0900

    add 숫자 색상 지정 색상 범위 로또 색상과 동일

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 const button = document.querySelector(".button");
+const yellow = "#FBC400";
+const blue = "#69C8F2";
+const red = "#FF7272";
+const gray = "#AAAAAA";
+const green = "#B0D840"
 button.addEventListener('click', ()=>{
     const numbers = document.querySelectorAll(".number");
     const lotto_number = [];
@@ -22,5 +27,28 @@ button.addEventListener('click', ()=>{
     //HTML내용에 숫자 적용
     for(let i=0; i<6; i++){
         numbers[i].innerText = lotto_number[i];
+        //숫자 크기 별 색상 적용
+        switch(parseInt((lotto_number[i]-1)/10)){
+            case 0:
+                //1~10 -> 노란색
+                numbers[i].style.backgroundColor = yellow;
+                break;
+            case 1:
+                //11~20 -> 파랑색
+                numbers[i].style.backgroundColor = blue;
+                break;
+            case 2:
+                //21~30 -> 빨강색
+                numbers[i].style.backgroundColor = red;
+                break;
+            case 3:
+                //31~40 -> 회색
+                numbers[i].style.backgroundColor = gray;
+                break;
+            case 4:
+                //41~45 -> 녹색
+                numbers[i].style.backgroundColor = green;
+                break;
+        }
     }
 })
